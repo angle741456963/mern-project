3fc1194eb0fad9601ad7da36e5bcffe38302c538
angle741456963 <angle741456963@gmail.com>
2025-09-27 13:33:57 +0800

    change to heroku APIURL

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/user";
+const API_URL = "https://mernproject-85d718f5fc22.herokuapp.com/api/user";
 
 class AuthService {
   login(email, password) {
